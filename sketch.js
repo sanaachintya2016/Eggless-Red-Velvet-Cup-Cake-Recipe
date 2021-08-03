26dabe8244482180fc02c16c20bfb9c772a98b20
Anik Sana <sanaachintya2016@gmail.com>
2021-08-03 20:32:45 +0530

    cake recipe

--- sketch.js
+++ sketch.js
@@ -1,9 +1,21 @@
 function setup() {
-  createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+  createCanvas(1000,1000);
 }
 
 function draw() {
-  background(255,255,255);  
+  background(255,255,255);
+  text(mouseX + ',' + mouseY, 30, 45);
+  fill("black")
+  text("400 gms (1 tin)Nestlé MILKMAID",500,100)
+  text ( "200 gmsMaida (All Purpose Flour)",500,125)
+  text("1 tbsp Cocoa Powder",500,150)
+  text ("1 tsp Baking Powder",500,175)
+  text ("1 tsp Soda Bicarbonate",500,200)
+  text ("100 gms Butter",500,225)
+  text ("1 tsp Vanilla Essence",500,250)
+  text("200 ml Aerated Soda",500,275)
+  text("2 tbsp Red Colour",500,300)
+  fill("darkblue")
+  text("Topping",550,335)
   drawSprites();
 }
